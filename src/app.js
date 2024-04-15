80df2b8d081763216c995943241ec6a1888a7224
Adriana Cabrera Escobar <160674673+AdriCabrera@users.noreply.github.com>
2024-04-15 00:12:45 +0000

    first commit Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "some"];
+  let adjs = ["great", "big", "cool", "awesome"];
+  let nouns = ["jogger", "racoon", "cat", "player"];
+  let domains = [".com", ".org", ".es", ".bo"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          domainNames.push(`${pronoun}${adj}${noun}${domain}`);
+        }
+      }
+    }
+  }
+  document.body.innerHTML = `<ol>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join(" ")}</ol>`;
 };
